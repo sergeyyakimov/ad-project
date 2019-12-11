356c17e0f7d3bc2c92c60f1fc60845a116cdcc79
SergeyYakimov <teriertis@yandex.ru>
2019-12-11 20:44:19 +0800

    Made orders and new-ad pages

--- src/views/Ads/NewAd.vue
+++ src/views/Ads/NewAd.vue
@@ -1,8 +1,74 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>New Ad!</h1>
+            <v-flex xs12 sm-6 offset-sm-3>
+                <h1 class="text--secondary mb-3">Create new ad</h1>
+                <v-form
+                        ref="form"
+                        v-model="valid"
+                        class="mb-3"
+                        validation
+                >
+                    <v-text-field
+                            label="Ad title"
+                            name="title"
+                            type="text"
+                            v-model="title"
+                            required
+                            :rules="[v => !!v || 'Title is required']"
+                    />
+                    <v-textarea
+                            name="description"
+                            v-model="description"
+                            :rules="[v => !!v || 'Description is required']"
+                    >
+                        <template v-slot:label>
+                            <div>
+                                Ad description
+                            </div>
+                        </template>
+                    </v-textarea>
+                </v-form>
+                <v-layout row>
+                    <v-flex xs12>
+                        <v-btn
+                                class="warning"
+                        >
+                            Upload
+                            <v-icon right dark>mdi-cloud-upload</v-icon>
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
+                <v-layout row class="mt-3">
+                    <v-flex xs12>
+                        <img
+                            src=""
+                            alt=""
+                            height="100"
+                        />
+                    </v-flex>
+                </v-layout>
+                <v-layout row class="mt-3">
+                    <v-flex xs12>
+                        <v-switch
+                            v-model="promo"
+                            label="Add to promo?"
+                            color="green"
+                        >
+                        </v-switch>
+                    </v-flex>
+                </v-layout>
+                <v-layout row class="mt-3">
+                    <v-flex xs12>
+                        <v-btn
+                                class="success"
+                                @click="createAd"
+                                :disabled="!valid"
+                        >
+                            Create ad
+                        </v-btn>
+                    </v-flex>
+                </v-layout>
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +77,22 @@
 <script>
     export default {
         data: () => ({
-
-        })
+            title: '',
+            description: '',
+            promo: false,
+            valid: false
+        }),
+        methods: {
+            createAd () {
+                if(this.$refs.form.validate()) {
+                    const ad ={
+                        title: this.title,
+                        description: this.description,
+                        promo: this.promo
+                    }
+                    console.log(ad)
+                }
+            }
+        }
     };
 </script>
--- src/views/User/Orders.vue
+++ src/views/User/Orders.vue
@@ -1,17 +1,67 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders!</h1>
+            <v-flex xs12 sm6 offset-sm-3>
+                <h1 class="text--secondary mb-3">Orders</h1>
+                <v-list
+                        subheader
+                        two-line
+                        flat
+                >
+                    <v-list-item-group
+                            multiple
+                    >
+                        <v-list-item
+                            v-for="order in orders"
+                            :key="order.id"
+                        >
+
+                                <v-list-item-action>
+                                    <v-checkbox
+                                            :input-value="order.done"
+                                            @change="markDone(order)"
+                                            color="success"
+                                    ></v-checkbox>
+                                </v-list-item-action>
+
+                                <v-list-item-content>
+                                    <v-list-item-title>{{order.name}}</v-list-item-title>
+                                    <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-btn
+                                            dark
+                                            class="green"
+                                            :to="'/ad/' + order.adId"
+                                    >
+                                        Open
+                                    </v-btn>
+                                </v-list-item-action>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-    export default {
-        data: () => ({
-
-        })
-    };
+export default {
+    data: () => ({
+        orders: [
+            {
+                id: 'a',
+                name: 'Sergey',
+                phone: '8-950-020-96-32',
+                adId: '123',
+                done: false
+            }
+        ]
+    }),
+    methods: {
+        markDone (order) {
+            order.done = true
+        }
+    }
+};
 </script>
